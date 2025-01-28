452e8fbe242a4bb666a6a4b8a110e5fe59b66b1a
dharmiksonani <dharmiksonani84618461@gmail.com>
2025-01-28 16:29:36 +0530

    Incoming Call Notification

--- Webrtc-App/src/App.js
+++ Webrtc-App/src/App.js
@@ -1,14 +1,65 @@
 import React, { useEffect } from 'react'
 import { StatusBar } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native';
+import { createNavigationContainerRef, NavigationContainer } from '@react-navigation/native';
 import { NavigationHandler } from './routes';
 import socketServices from './api/socketServices';
+import { useNotification } from './hooks/useNotification';
+import { Screens } from './routes/helper';
+import messaging from '@react-native-firebase/messaging';
+import InCallManager from 'react-native-incall-manager';
+import notifee, { EventType } from '@notifee/react-native';
+
+useNotification();
 
 const App = () => {
 
+  const navigationRef = createNavigationContainerRef(null);
+
   useEffect(() => {
-    socketServices.initializeSocket()
+    socketServices.initializeSocket();
+
+    const unsubscribeNotificationOpened = messaging().onNotificationOpenedApp(remoteMessage => {
+      // console.log('Notification caused app to open from background state:', remoteMessage);
+      handleNotificationPress(remoteMessage);
+    });
+
+    messaging()
+      .getInitialNotification()
+      .then(remoteMessage => {
+        if (remoteMessage) {
+          InCallManager.stopRingtone();
+          // console.log('Notification caused app to open from quit state:', remoteMessage);
+          handleNotificationPress(remoteMessage);
+        }
+      });
+
+    const unsubscribeNotifee = notifee.onForegroundEvent(({ type, detail }) => {
+      if (type === EventType.PRESS) {
+        // console.log('Notification pressed:', detail.notification.data);
+        handleNotificationPress(detail.notification.data);
+      }
+    });
+
+    return () => {
+      unsubscribeNotifee();
+      unsubscribeNotificationOpened();
+    };
   }, [])
+
+  const handleNotificationPress = (remoteMessage) => {
+    const data = remoteMessage?.data?.data && JSON.parse(remoteMessage?.data?.data);
+    if (data) {
+      const { from, to, offer } = data;
+      if (from && to && offer) {
+        navigationRef?.current?.navigate(Screens.VideoCallScreen, {
+          localUserId: to,
+          remoteUserId: from,
+          offer: offer,
+        });
+      }
+    }
+
+  };
 
   return (
     <>
@@ -17,7 +68,7 @@ const App = () => {
         translucent
         backgroundColor={'#00000000'}
       />
-      <NavigationContainer>
+      <NavigationContainer ref={navigationRef}>
         <NavigationHandler />
       </NavigationContainer>
     </>
---
+++ Webrtc-Server/src/notification/notification.js
@@ -0,0 +1,28 @@
+var admin = require("firebase-admin");
+
+const serviceAccount = require("../../webrtc-video-call-notification-firebase.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
+async function sendPushNotification(token, data = {}, title = 'WebRTC', body = 'Incoming Call Notification') {
+    const message = {
+        notification: {
+            title: title,
+            body: body,
+        },
+        token: token,
+        data: { data: JSON.stringify(data) },
+    };
+
+    try {
+        const response = await admin.messaging().send(message);
+        console.log('Notification sent successfully:', response);
+        return { success: true, response };
+    } catch (error) {
+        console.log('Error sending notification:', error);
+    }
+}
+
+module.exports = { sendPushNotification };

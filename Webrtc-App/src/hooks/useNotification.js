8c06491194b374972a2a363788d2884e7dc1d7f0
dharmiksonani <dharmiksonani84618461@gmail.com>
2025-01-28 18:23:44 +0530

    Accept Reject

--- Webrtc-App/src/hooks/useNotification.js
+++ Webrtc-App/src/hooks/useNotification.js
@@ -3,33 +3,10 @@ import notifee, { AndroidImportance } from '@notifee/react-native';
 import InCallManager from 'react-native-incall-manager';
 
 export const useNotification = () => {
-    messaging().setBackgroundMessageHandler(async remoteMessage => {
+    const displayCallNotification = async (remoteMessage) => {
         InCallManager.stopRingtone();
-        if (remoteMessage?.data?.data) {
-            // console.log('Message handled in the background!', remoteMessage.data.data && JSON.parse(remoteMessage.data.data));
-            InCallManager.startRingtone();
-        } else {
-            console.log('Message handled in the background!', remoteMessage);
-        }
-    });
+        InCallManager.startRingtone();
 
-    notifee.onBackgroundEvent((remoteMessage) => { })
-
-    messaging().getInitialNotification(async remoteMessage => {
-        InCallManager.stopRingtone();
-        if (remoteMessage?.data?.data) {
-            // console.log('Message handled in the Kill Mode', remoteMessage.data.data && JSON.parse(remoteMessage.data.data));
-            InCallManager.startRingtone();
-        } else {
-            console.log('Message handled in the Kill Mode', remoteMessage);
-        }
-    });
-
-    messaging().onMessage(async remoteMessage => {
-        // Request permissions (required for iOS)
-        await notifee.requestPermission();
-        InCallManager.stopRingtone();
-        // Create a channel (required for Android)
         const channelId = await notifee.createChannel({
             id: remoteMessage.from.toString(),
             name: 'WebRTC',
@@ -38,21 +15,45 @@ export const useNotification = () => {
             importance: AndroidImportance.HIGH,
         });
 
-        if (remoteMessage?.data?.data) {
-            // console.log('Message handled', remoteMessage.data.data && JSON.parse(remoteMessage.data.data));
-            InCallManager.startRingtone();
-        } else {
-            console.log('Message handled', remoteMessage);
-        }
-
-        // Display a notification
         await notifee.displayNotification({
-            title: remoteMessage.notification.title.toString(),
-            body: remoteMessage.notification.body.toString(),
+            title: remoteMessage.notification?.title || 'Incoming Call',
+            body: remoteMessage.notification?.body || 'You have an incoming call',
             android: {
                 channelId,
+                importance: AndroidImportance.HIGH,
+                ongoing: true,
+                autoCancel: false,
+                actions: [
+                    {
+                        title: 'Accept',
+                        pressAction: { id: 'accept' },
+                    },
+                    {
+                        title: 'Reject',
+                        pressAction: { id: 'reject' },
+                    },
+                ],
             },
             data: remoteMessage,
         });
+    };
+
+    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+        // console.log('Background message received:', remoteMessage);
+        // onNotificationReceive(remoteMessage);
     });
+
+    messaging().onMessage(async (remoteMessage) => {
+        // console.log('Foreground message received:', remoteMessage);
+        onNotificationReceive(remoteMessage);
+    });
+
+    const onNotificationReceive = async (remoteMessage) => {
+        const data = remoteMessage?.data?.data && JSON.parse(remoteMessage?.data?.data);
+        if (data?.type === 'call') {
+            await displayCallNotification(remoteMessage);
+        }
+    }
+
+    notifee.onBackgroundEvent(async ({ type, detail }) => { });
 };
